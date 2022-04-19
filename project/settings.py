01e29ad17dff9f1095df320050ecb66c2140fcf5
Jessica <jessicamwangi933@gmail.com>
2022-04-19 08:47:53 +0300

    added email and registration info

--- project/settings.py
+++ project/settings.py
@@ -148,3 +148,9 @@ django_heroku.settings(locals())
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL ='/accounts/profile/'
+
+EMAIL_USE_TLS=config('EMAIL_USE_TLS')
+EMAIL_HOST=config('EMAIL_HOST')
+EMAIL_PORT=config('EMAIL_PORT')
+EMAIL_HOST_USER=config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
