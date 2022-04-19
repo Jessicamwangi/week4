5d407cddb4a2cfdb1ed45148fe19de8c25cbf375
Jessica <jessicamwangi933@gmail.com>
2022-04-19 08:36:06 +0300

    added my database

--- project/settings.py
+++ project/settings.py
@@ -83,8 +83,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neighbour',
+        'USER': 'jessica',
+    '   PASSWORD':'Jesna7403',
     }
 }
 
